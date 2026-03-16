48cee1ae84ed1ded4e2c2ef1344de12fecb711b4
sudipta.shahin <sudipta.shahin@dreamonline.co.jp>
2026-03-16 18:05:23 +0600

    update next.config.ts -> .js

---
+++ frontend/next.config.js
@@ -0,0 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'standalone',  // newly added for production
+//   assetPrefix: '',  // usually blank for root deployment
+//   reactCompiler: true,
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
+  /* config options here */
+  output: 'standalone',  // newly added for production
+  assetPrefix: '',  // usually blank for root deployment
+  reactCompiler: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
